49884f428f669b246016bb5c01028cf10709f19d
jaeyeun <jaeyeun95@gmail.com>
2021-10-11 22:22:54 +0900

    다형성 활용과 다운캐스팅(4)

--- src/chapter8/inheritance/AnimalTest.java
+++ src/chapter8/inheritance/AnimalTest.java
@@ -12,17 +12,29 @@ class Human extends Animal {
     public void move() {
         System.out.println("사람이 두발로 걷습니다.");
     }
+
+    public void readBook() {
+        System.out.println("사람이 책을 읽습니다.");
+    }
 }
 
 class Tiger extends Animal {
     public void move() {
         System.out.println("사람이 네발로 뜁니다.");
     }
+
+    public void hunting() {
+        System.out.println("호랑이가 사냥을 합니다.");
+    }
 }
 
 class Eagle extends Animal {
     public void move() {
         System.out.println("독수리가 하늘을 납니다.");
+    }
+
+    public void flying() {
+        System.out.println("하늘을 날아갑니다.");
     }
 }
 
@@ -49,6 +61,24 @@ public class AnimalTest {
 
         animal.move();
 
-        Human h = (Human) animal;
+        // 휴먼의 readBook()은 호출 되지만 뒤에 꺼에서 에러가 남
+        // Human human = (Human) animal;
+        // human.readBook();
+
+        if (animal instanceof Human) {
+            Human human = (Human) animal;
+            human.readBook();
+        } else if (animal instanceof Human) {
+            Tiger tiger = (Tiger) animal;
+            tiger.hunting();
+        } else if (animal instanceof Eagle) {
+            Eagle eagle = (Eagle) animal;
+            eagle.flying();
+        } else {
+            System.out.println("지원되지 않는 기능입니다.");
+        }
+
+        // Object는 모든 클래스의 상위 클래스
+
     }
 }
